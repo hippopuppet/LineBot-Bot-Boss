31c43a8628b546e2da487522d11d08ecc52c8bbb
hippopuppet <35716029+hippopuppet@users.noreply.github.com>
2018-01-24 13:13:35 +0800

    TEST

--- main.go
+++ main.go
@@ -24,6 +24,9 @@ import (
 	"strings"
 
 	"github.com/line/line-bot-sdk-go/linebot"
+	"cloud.google.com/go/storage"
+	"google.golang.org/appengine"
+    "google.golang.org/appengine/file"
 )
 type Page struct {
     KingOfName  string `json:"kingofname"`
@@ -57,6 +60,100 @@ func getPages() []Page {
     json.Unmarshal(raw, &c)
     return c
 }
+
+// demo struct holds information needed to run the various demo functions.
+type demo struct {
+	client     *storage.Client
+	bucketName string
+	bucket     *storage.BucketHandle
+
+	w   io.Writer
+	ctx context.Context
+	// cleanUp is a list of filenames that need cleaning up at the end of the demo.
+	cleanUp []string
+	// failed indicates that one or more of the demo steps failed.
+	failed bool
+}
+
+func (d *demo) errorf(format string, args ...interface{}) {
+	d.failed = true
+	fmt.Fprintln(d.w, fmt.Sprintf(format, args...))
+	log.Errorf(d.ctx, format, args...)
+}
+
+//[START write]
+// createFile creates a file in Google Cloud Storage.
+func (d *demo) createFile(fileName string) {
+	fmt.Fprintf(d.w, "Creating file /%v/%v\n", d.bucketName, fileName)
+
+	wc := d.bucket.Object(fileName).NewWriter(d.ctx)
+	wc.ContentType = "text/plain"
+	wc.Metadata = map[string]string{
+		"x-goog-meta-foo": "foo",
+		"x-goog-meta-bar": "bar",
+	}
+	d.cleanUp = append(d.cleanUp, fileName)
+
+	if _, err := wc.Write([]byte("abcde\n")); err != nil {
+		d.errorf("createFile: unable to write data to bucket %q, file %q: %v", d.bucketName, fileName, err)
+		return
+	}
+	if _, err := wc.Write([]byte(strings.Repeat("f", 1024*4) + "\n")); err != nil {
+		d.errorf("createFile: unable to write data to bucket %q, file %q: %v", d.bucketName, fileName, err)
+		return
+	}
+	if err := wc.Close(); err != nil {
+		d.errorf("createFile: unable to close bucket %q, file %q: %v", d.bucketName, fileName, err)
+		return
+	}
+}
+
+//[END write]
+
+//[START read]
+// readFile reads the named file in Google Cloud Storage.
+func (d *demo) readFile(fileName string) {
+	io.WriteString(d.w, "\nAbbreviated file content (first line and last 1K):\n")
+
+	rc, err := d.bucket.Object(fileName).NewReader(d.ctx)
+	if err != nil {
+		d.errorf("readFile: unable to open file from bucket %q, file %q: %v", d.bucketName, fileName, err)
+		return
+	}
+	defer rc.Close()
+	slurp, err := ioutil.ReadAll(rc)
+	if err != nil {
+		d.errorf("readFile: unable to read data from bucket %q, file %q: %v", d.bucketName, fileName, err)
+		return
+	}
+
+	fmt.Fprintf(d.w, "%s\n", bytes.SplitN(slurp, []byte("\n"), 2)[0])
+	if len(slurp) > 1024 {
+		fmt.Fprintf(d.w, "...%s\n", slurp[len(slurp)-1024:])
+	} else {
+		fmt.Fprintf(d.w, "%s\n", slurp)
+	}
+}
+
+//[END read]
+
+//[START copy]
+// copyFile copies a file in Google Cloud Storage.
+func (d *demo) copyFile(fileName string) {
+	copyName := fileName + "-copy"
+	fmt.Fprintf(d.w, "Copying file /%v/%v to /%v/%v:\n", d.bucketName, fileName, d.bucketName, copyName)
+
+	obj, err := d.bucket.Object(copyName).CopierFrom(d.bucket.Object(fileName)).Run(d.ctx)
+	if err != nil {
+		d.errorf("copyFile: unable to copy /%v/%v to bucket %q, file %q: %v", d.bucketName, fileName, d.bucketName, copyName, err)
+		return
+	}
+	d.cleanUp = append(d.cleanUp, copyName)
+
+	d.dumpStats(obj)
+}
+
+//[END copy]
 
 var bot *linebot.Client
 var userID string
@@ -154,11 +251,37 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 									}
 								}
 								pagesJson, _ := json.Marshal(pages)
-								err = ioutil.WriteFile("C:\\BossRefreshInfo.json", pagesJson, os.O_CREATE)
+								err = ioutil.WriteFile("BossRefreshInfo.json", pagesJson, 0644)
 								if err != nil {
 									log.Println(err)
 								}
 								log.Println("WriteFile ...."+string(pagesJson) )
+								//[START get_default_bucket]
+								// Use `dev_appserver.py --default_gcs_bucket_name GCS_BUCKET_NAME`
+								// when running locally.
+								bucket, err := file.DefaultBucketName(ctx)
+								if err != nil {
+									log.Errorf(ctx, "failed to get default GCS bucket name: %v", err)
+								}
+								//[END get_default_bucket]
+								client, err := storage.NewClient(ctx)
+								if err != nil {
+									log.Errorf(ctx, "failed to create client: %v", err)
+									return
+								}
+								defer client.Close()
+								
+								buf := &bytes.Buffer{}
+								d := &demo{
+									w:          buf,
+									ctx:        ctx,
+									client:     client,
+									bucket:     client.Bucket(bucket),
+									bucketName: bucket,
+								}
+
+								n := "demo-testfile-go"
+								d.createFile(n)
 							}
 						}
 					}
