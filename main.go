0f0e2780afc5b45d35e19737aaad848cc6124bd4
hippopuppet <35716029+hippopuppet@users.noreply.github.com>
2018-01-25 15:56:07 +0800

    TTEST

--- main.go
+++ main.go
@@ -234,11 +234,13 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 										log.Println("calaculate resurrection .... "+ dbResult[0].BossInfo[i].Resurrection)
 
 										// Update
-										colQuerier := bson.M{ "BOSSINFO": {"kingofname": dbResult[0].BossInfo[i].KingOfName} }
+										updateData := BOSSINFO{"die": dbResult[0].BossInfo[i].Die,"resurrection": dbResult[0].BossInfo[i].Resurrection}
+										colQuerier := bson.M{"kingofname": dbResult[0].BossInfo[i].KingOfName} 
 										log.Print("kingofname: ")
 										log.Println(colQuerier)
 
-										change := bson.M{"$set": bson.M{"die": dbResult[0].BossInfo[i].Die, "resurrection": dbResult[0].BossInfo[i].Resurrection}}
+										//change := bson.M{"$set": bson.M{"die": dbResult[0].BossInfo[i].Die, "resurrection": dbResult[0].BossInfo[i].Resurrection}}
+										change := bson.M{"$set": bson.M{"BOSSINFO": updateData}}
 										err = c.Update(colQuerier, change)
 										if err != nil {
 											panic(err)
