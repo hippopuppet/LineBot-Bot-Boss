bd051f9aa086dac6e548479b6e7aad273e24f70b
hippopuppet <35716029+hippopuppet@users.noreply.github.com>
2018-01-25 10:09:47 +0800

    TEST

--- main.go
+++ main.go
@@ -185,7 +185,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					log.Println(dbResult)
 					
 					log.Println("BOSSINFO: ...")
-					log.Println(dbResult[0]["BOSSINFO"].(map[string]string))
+					log.Println(dbResult["BOSSINFO"].(map[string]string))
     
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+"--"+ strconv.Itoa( time.Now().In(local).Hour() )+"-"+strconv.Itoa( time.Now().In(local).Minute() )+"-"+strconv.Itoa( time.Now().In(local).Second() ) )).Do(); err != nil {
 						log.Print(err)
