605bf1e29e89505f4535bc95d01881821a588be8
hippopuppet <35716029+hippopuppet@users.noreply.github.com>
2018-01-26 21:55:03 +0800

    T

--- main.go
+++ main.go
@@ -383,7 +383,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				dbResult[0].GroupInfo[i].Id =  event.Source.GroupID
 				dbResult[0].GroupInfo[i].Type = "user"
 				dbResult[0].GroupInfo[i].Active = 0
-				colQuerier := bson.M{"GROUPINFO.id" : event.Source.UserID}
+				//colQuerier := bson.M{"GROUPINFO.id" : event.Source.UserID}
 				upsertData := bson.M{"$set": bson.M{"GROUPINFO":  bson.M{"id": dbResult[0].GroupInfo[i].Id, "type": dbResult[0].GroupInfo[i].Type, "active":dbResult[0].GroupInfo[i].Active}}}
 				info, err := c.UpsertId(bson.M{"_id": "5a69aa488d0d213fd88abd95"}, upsertData)
 				if err != nil {
