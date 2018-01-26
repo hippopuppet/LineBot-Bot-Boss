745bec497951bd65ac5ee68d85035ba8ca9f7480
hippopuppet <35716029+hippopuppet@users.noreply.github.com>
2018-01-26 12:28:41 +0800

    T

--- main.go
+++ main.go
@@ -315,15 +315,23 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			// Optional. Switch the session to a monotonic behavior.
 			session.SetMode(mgo.Monotonic, true)
 			c := session.DB("heroku_xzzlp7s1").C("bossinfo")
-			// Upsert
-			colQuerier := bson.M{"GROUPINFO.id": event.Source.GroupID}
-			change := bson.M{"$set": bson.M{"GROUPINFO.$.id": event.Source.GroupID, "GROUPINFO.$.type": "GROUP", "GROUPINFO.$.active": 0}}
-			//id := bson.ObjectIdHex("5a69a0718d0d213fd88abd92")
-			info, err := c.Upsert(colQuerier, change)
+			//Find
+			var dbResult bson.M
+			err := c.Find(bson.M{"GROUPINFO.id": event.Source.GroupID}).One(&dbResult)
 			if err != nil {
 				panic(err)
 			}
-			log.Print(info)
+			log.Print("dbResult")
+			log.Println(dbResult)
+			/*if dbResult 
+			// Insert
+			insetData := bson.M{"GROUPINFO.$.id": event.Source.GroupID, "GROUPINFO.$.type": "GROUP", "GROUPINFO.$.active": 0}
+			err := c.Insert(insetData)
+			if err != nil {
+				panic(err)
+			}
+			*/
+			
 			if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(event.Source.GroupID)).Do(); err != nil {
 					log.Print(err)
 			}
