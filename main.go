f108f8502a7ea495f6cea28f19571bb18b315ac9
hippopuppet <35716029+hippopuppet@users.noreply.github.com>
2018-01-27 22:17:13 +0800

    T

--- main.go
+++ main.go
@@ -99,7 +99,7 @@ func toJson(p interface{}) string {
     return string(bytes)
 }
 
-func getAirJson(result *AIRINFO) error {
+func getAirJson(result AIRINFO) error {
 	url := "http://opendata2.epa.gov.tw/AQI.json"
 	resp, err := http.Get(url)
     if err != nil {
@@ -112,7 +112,7 @@ func getAirJson(result *AIRINFO) error {
     }
     // We could check the resulting content type
     // here if desired.
-    err = json.NewDecoder(resp.Body).Decode(result)
+    err = json.NewDecoder(resp.Body).Decode(&result)
 	log.Println("result")
 	log.Println(result)
     if err != nil {
@@ -375,7 +375,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					}//!LIST
 					if message.Text == "!PM" {
 						airJson := AIRINFO{}
-						getAirJson(&airJson)
+						getAirJson(airJson)
 						log.Println(airJson)
 					}
 
