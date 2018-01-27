129a9b8af5f4e3d845ceae46a7245a0431032fba
hippopuppet <35716029+hippopuppet@users.noreply.github.com>
2018-01-27 12:05:17 +0800

    T

--- main.go
+++ main.go
@@ -381,7 +381,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			}
 			// Upsert
 			index := len(dbResult[0].GroupInfo)
-			if index <= 0 {
+			/*if index <= 0 {
 				log.Println("index <= 0 ")
 				upsertData := bson.M{"$push": bson.M{"GROUPINFO": bson.M{"id": event.Source.UserID, "type": "user", "active":0}}}
 				//upsertData := bson.M{"$push": bson.M{"GROUPINFO.$.id": event.Source.UserID, "GROUPINFO.$.type": "user", "GROUPINFO.$.active":0}}
@@ -390,18 +390,26 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					log.Println(err)
 				}
 				log.Println(info)
-			}
-			if index > 0 {
-				log.Println("index > 0 ")
-				colQuerier := bson.M{"GROUPINFO.id" : event.Source.UserID}
-				upsertData := bson.M{"$set": bson.M{"GROUPINFO.$.id": event.Source.UserID, "GROUPINFO.$.type": "user", "GROUPINFO.$.active":0}}
-				//upsertData := bson.M{"$set": bson.M{"GROUPINFO": bson.M{ "id": event.Source.UserID, "type": "user", "active":0}}}
-				info, err := c.Upsert(colQuerier, upsertData)
-				if err != nil {
-					log.Println(err)
+			}*/
+			
+			log.Println("index > 0 ")
+			colQuerier := bson.M{"GROUPINFO.id" : event.Source.UserID}
+			upsertData := bson.M{"$set": bson.M{"GROUPINFO.$.id": event.Source.UserID, "GROUPINFO.$.type": "user", "GROUPINFO.$.active":0}}
+			//upsertData := bson.M{"$set": bson.M{"GROUPINFO": bson.M{ "id": event.Source.UserID, "type": "user", "active":0}}}
+			info, err := c.Upsert(colQuerier, upsertData)
+			if err != nil {
+				log.Println(err)
+				if err == ErrNotFound {
+					upsertData := bson.M{"$push": bson.M{"GROUPINFO": bson.M{"id": event.Source.UserID, "type": "user", "active":0}}}
+					info, err := c.UpsertId(bson.ObjectIdHex("5a69aa488d0d213fd88abd95"), upsertData)
+					if err != nil {
+						log.Println(err)
+					}
+					log.Println(info)
 				}
-				log.Println(info)
 			}
+			log.Println(info)
+			
 		
 		originalContentURL := "https://i.imgur.com/Qr2DKSG.jpg"
 		previewImageURL := "https://i.imgur.com/Qr2DKSG.jpg"
