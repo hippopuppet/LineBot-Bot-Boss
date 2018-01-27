b70349b9921a9ed4e3c89d6ed90292535c90ee9d
hippopuppet <35716029+hippopuppet@users.noreply.github.com>
2018-01-28 00:18:34 +0800

    T

--- main.go
+++ main.go
@@ -363,10 +363,9 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							list_buf.WriteString(" : ")
 							aResurrection := strconv.Itoa(bossinfo.Resurrection)
 							lens := len(aResurrection)
-							for  i := 0 ; i < 4-lens ; ++i {
+							for  i := 0 ; i < 4-lens ; i++ {
 								list_buf.WriteString("0")
 							}
-
 							list_buf.WriteString(bossinfo.Resurrection)
 							list_buf.WriteString("   Map: ")
 							list_buf.WriteString(bossinfo.Map)
