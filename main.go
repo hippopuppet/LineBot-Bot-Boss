c5cae089e5b2296b133d729463af65c032a29000
hippopuppet <35716029+hippopuppet@users.noreply.github.com>
2018-01-27 23:22:30 +0800

    T

--- main.go
+++ main.go
@@ -380,9 +380,9 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 								if airinfo.SiteName == result[1] {
 									var airinfo_buf bytes.Buffer
 									airinfo_buf.WriteString(airinfo.SiteName)
-									airinfo_buf.WriteString("ªº PM2.5 ¼Æ­È¬° ")
+									airinfo_buf.WriteString("çš„ PM2.5 æ•¸å€¼ç‚º ")
 									airinfo_buf.WriteString(airinfo.PM2_5)
-									airinfo_buf.WriteString("ª¬ºA ")
+									airinfo_buf.WriteString("ç‹€æ…‹ ")
 									airinfo_buf.WriteString(airinfo.Status)
 
 									if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(airinfo_buf.String())).Do(); err != nil {
@@ -393,15 +393,15 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							}
 							if isFound == false {
 								var airinfo_buf bytes.Buffer
-								airinfo_buf.WriteString("¨S¦³")
+								airinfo_buf.WriteString("æ²’æœ‰")
 								airinfo_buf.WriteString(result[1])
-								airinfo_buf.WriteString("ªº PM2.5 ¸ê®Æ")
+								airinfo_buf.WriteString("çš„ PM2.5 è³‡æ–™")
 								if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(airinfo_buf.String())).Do(); err != nil {
 										log.Print(err)
 								}
 							}
 						} else {
-							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("½Ð¿é¤J±ý¬d¸ßPM2.5¸ê®Æªº¦aÂI")).Do(); err != nil {
+							if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("è«‹è¼¸å…¥æ¬²æŸ¥è©¢PM2.5è³‡æ–™çš„åœ°é»ž")).Do(); err != nil {
 									log.Print(err)
 							}
 						}
