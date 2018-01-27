8661dac17da95b4c07f4bcf5c26d1160c3b2e161
hippopuppet <35716029+hippopuppet@users.noreply.github.com>
2018-01-27 21:57:58 +0800

    T

--- main.go
+++ main.go
@@ -99,7 +99,7 @@ func toJson(p interface{}) string {
     return string(bytes)
 }
 
-func getAirJson(target interface{}) error {
+func getAirJson(result interface{}) error {
 	url := "http://opendata2.epa.gov.tw/AQI.json"
 	resp, err := http.Get(url)
     if err != nil {
