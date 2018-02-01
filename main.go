fdcae038b7002e2b77fe1f31c9e98602248a381d
hippopuppet <35716029+hippopuppet@users.noreply.github.com>
2018-02-01 10:37:57 +0800

    T

--- main.go
+++ main.go
@@ -525,10 +525,12 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					var stockJson []STOCKINFO
 					var stockresult interface{}
 
-					_, err := url.Parse("http://mis.twse.com.tw/stock/fibest.jsp?stock=1101")
+					u, err := url.Parse("http://mis.twse.com.tw/stock/fibest.jsp?stock=1101")
 					if err != nil {
 						log.Fatal(err)
 					}
+					_ := u.Query()
+
 					URL := "http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_"+stocknum+".tw&json=1&delay=0&d="+date_buf.String()+"&_="+CURRENT_MILLIS
 					log.Print("URL")
 					log.Print(URL)
